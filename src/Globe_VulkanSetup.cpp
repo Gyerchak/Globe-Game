c019b42ef730c7ba276b3d8ab7580d4b20f7e452
Gyerchak <gyerchak2000@gmail.com>
2026-08-02 14:00:20 +0200

    Add Globe_VulkanSetup: implement Vulkan instance creation and physical device selection methods

---
+++ src/Globe_VulkanSetup.cpp
@@ -0,0 +1,11 @@
+#include "GlobeApp.h"
+
+void GlobeApp::createInstance() {
+    // ... original code
+}
+
+void GlobeApp::pickPhysicalDevice() {
+    // ... prints GPU info and max texture size
+}
+
+// ... other functions
